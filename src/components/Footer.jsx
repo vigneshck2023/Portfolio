3d7459bb48791744ba1d722c382655b76d30e9ae
Vignesh C K <vigneshck2003.code@gmail.com>
2025-10-10 09:00:01 +0530

    including info in the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,28 +6,28 @@ export default function Footer() {
     <footer className="footer-simple">
       <div className="social-icons">
         <a
-          href="https://linkedin.com/in/yourusername"
+          href="https://www.linkedin.com/in/vignesh-c-k-8a2528201/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaLinkedin />
         </a>
         <a
-          href="https://github.com/yourusername"
+          href="https://github.com/vigneshck2023"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaGithub />
         </a>
         <a
-          href="mailto:yourmail@gmail.com"
+          href="mailto:vigneshck2003@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaEnvelope />
         </a>
         <a
-          href="https://twitter.com/yourusername"
+          href="https://x.com/vigneshck2003"
           target="_blank"
           rel="noopener noreferrer"
         >
